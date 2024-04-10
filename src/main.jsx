a7db7133383ca29d94390e8feb2567b5b236ea7f
Omar <118843137+Rakions@users.noreply.github.com>
2024-04-11 00:03:49 +0200

    Instalar PrimeReact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="bg-slate-500">hola</div>
+    <PrimeReactProvider>
+      <div className="bg-slate-500">hola</div>
+    </PrimeReactProvider>
   </React.StrictMode>
 );
