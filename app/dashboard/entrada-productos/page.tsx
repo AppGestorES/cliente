b43865d1927ac0bb0f605e49daa1c7fc16f55d8e
Omar <118843137+Rakions@users.noreply.github.com>
2024-06-04 01:00:41 +0200

    Modal generico y tabla generica

--- app/dashboard/entrada-productos/page.tsx
+++ app/dashboard/entrada-productos/page.tsx
@@ -1,42 +1,55 @@
 "use client";
 
-import { Button } from "primereact/button";
-import { useState } from "react";
-import { z } from "zod";
-import ModalEntradaProductos from "@/app/Components/entrada-productos/modalEntradaProductos";
-import TablaProductos from "@/app/Components/entrada-productos/tablaProductos";
-import { useDispatch } from "react-redux";
-import { deleteEntradaProductos } from "@/app/redux/slices/entradaProductosSlice";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    fetchEntradaProductos,
+    postEntradaProductos,
+    putEntradaProductos,
+    deleteEntradaProductos,
+} from "@/app/redux/slices/entradaProductosSlice";
+import GenericTable from "@/app/Components/generics/GenericTable";
+import GenericModal from "@/app/Components/generics/GenericModal";
 import { EntradaDeProductos } from "@/app/interfaces/EntradaProductos";
-import { AppDispatch } from "@/app/redux/store";
+import { AppDispatch, RootState } from "@/app/redux/store";
 import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
+import { Button } from "primereact/button";
 
-const invalid_type_error =
-    "Tipo de dato inválido proporcionado para este campo";
-const required_error = "Este campo no puede estar vacío";
+const EntradaProductosPage: React.FC = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const productos = useSelector(
+        (state: RootState) => state.entradaProductos.productos
+    );
+    const status = useSelector(
+        (state: RootState) => state.entradaProductos.status
+    );
+    const error = useSelector(
+        (state: RootState) => state.entradaProductos.error
+    );
 
-export const FormSchema = z.object({
-    producto: z
-        .string({ invalid_type_error, required_error })
-        .min(1, { message: required_error }),
-});
-
-const EntradaProductos: React.FC = () => {
-    const [visible, setVisible] = useState(false);
-    const [botonEliminar, setBotonEliminar] = useState(false);
     const [selectedProducts, setSelectedProducts] = useState<
         EntradaDeProductos[]
     >([]);
+    const [modalVisible, setModalVisible] = useState(false);
     const [selectedProduct, setSelectedProduct] =
         useState<EntradaDeProductos | null>(null);
-    const dispatch: AppDispatch = useDispatch();
+
+    useEffect(() => {
+        if (status === "idle") {
+            dispatch(fetchEntradaProductos());
+        }
+    }, [status, dispatch]);
+
+    const handleEdit = (product: EntradaDeProductos) => {
+        setSelectedProduct(product);
+        setModalVisible(true);
+    };
 
     const handleDelete = () => {
         if (selectedProducts.length > 0) {
             const idsToDelete = selectedProducts.map((product) => product.id);
             dispatch(deleteEntradaProductos(idsToDelete));
             setSelectedProducts([]);
-            setBotonEliminar(false);
         }
     };
 
@@ -53,13 +66,28 @@ const EntradaProductos: React.FC = () => {
         });
     };
 
+    const handleModalSubmit = (product: EntradaDeProductos) => {
+        if (selectedProduct) {
+            dispatch(
+                putEntradaProductos({
+                    id: selectedProduct.id,
+                    updatedProduct: product,
+                })
+            );
+        } else {
+            dispatch(postEntradaProductos(product));
+        }
+        setSelectedProduct(null);
+        setModalVisible(false);
+    };
+
     return (
         <div className="w-full">
             <ConfirmDialog />
             <div className="flex flex-col md:flex-row w-full md:items-center justify-between px-4">
                 <h2 className="text-xl">Entrada de Productos</h2>
                 <div className="flex gap-2 items-center">
-                    {botonEliminar && (
+                    {selectedProducts.length > 0 && (
                         <Button
                             label="Eliminar seleccionados"
                             icon="pi pi-trash"
@@ -71,26 +99,113 @@ const EntradaProductos: React.FC = () => {
                         label="Añadir producto"
                         icon="pi pi-plus"
                         className="bg-[var(--surface-a)] p-2 hover:bg-[var(--primary-color)] mt-2 max-w-[200px]"
-                        onClick={() => {
-                            setSelectedProduct(null);
-                            setVisible(true);
-                        }}
+                        onClick={() => setModalVisible(true)}
                     />
                 </div>
             </div>
-            <ModalEntradaProductos
-                visible={visible}
-                setVisible={setVisible}
-                selectedProduct={selectedProduct}
+            <GenericTable
+                data={productos}
+                columns={[
+                    { field: "id", header: "ID" },
+                    { field: "producto_final_id", header: "Producto Final Id" },
+                    {
+                        field: "fecha_entrada",
+                        header: "Fecha Entrada",
+                        render: (rowData) =>
+                            new Date(
+                                rowData.fecha_entrada * 1000
+                            ).toLocaleDateString(),
+                    },
+                    { field: "proveedor", header: "Proveedor" },
+                    { field: "numero_albaran", header: "Número Albaran" },
+                    { field: "numero_lote", header: "Número de lote" },
+                    { field: "cantidad_kg", header: "Cantidad (KG)" },
+                    {
+                        field: "fecha_caducidad",
+                        header: "Fecha Caducidad",
+                        render: (rowData) =>
+                            rowData.fecha_caducidad
+                                ? new Date(
+                                      rowData.fecha_caducidad * 1000
+                                  ).toLocaleDateString()
+                                : "",
+                    },
+                    { field: "envasado_id", header: "ID Envasado" },
+                    { field: "operario_id", header: "ID Operario" },
+                    { field: "id_proyecto", header: "ID Proyecto" },
+                ]}
+                selectedItems={selectedProducts}
+                setSelectedItems={setSelectedProducts}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+                loading={status === "loading"}
+                error={error}
             />
-            <TablaProductos
-                setSelectedProducts={setSelectedProducts}
-                setBotonEliminar={setBotonEliminar}
-                setVisible={setVisible}
-                setSelectedProduct={setSelectedProduct}
+            <GenericModal
+                visible={modalVisible}
+                setVisible={setModalVisible}
+                initialValues={
+                    selectedProduct || {
+                        id: 0,
+                        producto_final_id: 0,
+                        fecha_entrada: Math.floor(Date.now() / 1000),
+                        proveedor: "",
+                        numero_albaran: "",
+                        numero_lote: "",
+                        cantidad_kg: 0,
+                        fecha_caducidad: 0,
+                        envasado_id: 0,
+                        operario_id: 0,
+                        id_proyecto: 1,
+                    }
+                }
+                onSubmit={handleModalSubmit}
+                fields={[
+                    {
+                        key: "producto_final_id",
+                        label: "ID del Producto Final",
+                        type: "number",
+                    },
+                    {
+                        key: "fecha_entrada",
+                        label: "Fecha de entrada",
+                        type: "date",
+                    },
+                    { key: "proveedor", label: "Proveedor", type: "text" },
+                    {
+                        key: "numero_albaran",
+                        label: "Número de albarán",
+                        type: "text",
+                    },
+                    {
+                        key: "numero_lote",
+                        label: "Número de lote",
+                        type: "text",
+                    },
+                    {
+                        key: "cantidad_kg",
+                        label: "Cantidad (KG)",
+                        type: "number",
+                    },
+                    {
+                        key: "fecha_caducidad",
+                        label: "Fecha de caducidad",
+                        type: "date",
+                    },
+                    {
+                        key: "envasado_id",
+                        label: "ID Envasado",
+                        type: "number",
+                    },
+                    {
+                        key: "operario_id",
+                        label: "ID Operario",
+                        type: "number",
+                    },
+                ]}
             />
         </div>
     );
 };
 
-export default EntradaProductos;
+export default EntradaProductosPage;
