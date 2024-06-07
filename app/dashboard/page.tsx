c894cfe725a161de6e3999fad6bc744c8ee972b6
Omar <118843137+Rakions@users.noreply.github.com>
2024-06-07 16:46:14 +0200

    Instalar chart.js

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { Chart } from "primereact/chart";
 
 export default function Dashboard() {
     return (
         <div>
-            <h1>Dashboard Content</h1>
+            <h1>Dashboard</h1>
         </div>
     );
 }
