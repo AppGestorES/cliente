b5d72977bb1945a96c56e6533ad40599becc8c18
Zas <thezasm03@gmail.com>
2024-06-10 17:00:32 +0200

    Ultimo arreglo final

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
+import withAuth from "./dashboard/withAuth";
 import "./globals.scss";
-import "@/app/utils/themes/viva-dark/theme.css";
+import "@/public/themes/viva-dark/theme.css";
+import { ProgressSpinner } from 'primereact/progressspinner';
 
-export default function Home() {
+const Home = () => {
     return (
-        <div>
-            <h1>Hola</h1>
+        <div className="flex flex-col gap-8 justify-center items-center h-[100vh]">
+            <h1 className="font-semibold text-4xl">Esperando a que se te asigne un proyecto...</h1>
+            <ProgressSpinner />
         </div>
     );
 }
+
+export default Home;
